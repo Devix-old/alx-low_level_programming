1bafec9e531748ab263f04a175ec98fe26224681
devix-old <mohamed.elgharef@gmail.com>
2023-03-16 15:37:23 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-/**
- * Description
- * print a message in the console
- * and return0
- */
+/*initial description*/
 int main(void)
+	/*main description*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	/*return 0 */
 	return (0);
 }
