f077361bfaa93a50a0048d8e9948eafed5ffa7fb
devix-old <mohamed.elgharef@gmail.com>
2023-03-17 10:15:05 -0700

    0x00. C - Hello, World

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there*/
+/**
+ * main - print alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+putchar ("abcdefghijklmnopqrstuvwxyz");
+	return (0);
+}
