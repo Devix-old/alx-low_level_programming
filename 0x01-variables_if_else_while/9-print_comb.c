bfac2e5570c0ceb496d3bb77b9eabb2bb982eac9
devix-old <mohamed.elgharef@gmail.com>
2023-03-17 11:36:13 -0700

    0x00. C - Hello, World

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there*/
+/**
+ * main - prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0 ; i < 9 ; i++)
+{
+putchar (i + '0');
+putchar(',');
+putchar(' ');
+}
+putchar(9 + '0');
+}
+return (0);
+}
