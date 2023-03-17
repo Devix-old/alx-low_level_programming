cee1a930e584a4ab9699469041b8879b65d568a1
devix-old <mohamed.elgharef@gmail.com>
2023-03-17 10:52:09 -0700

    0x00. C - Hello, World

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there*/
+/**
+ * main - print alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char str[] = "0123456789";
+int i = 0;
+for (i = 0 ; i < 10 ; i++)
+{
+putchar (str[i]);
+}
+putchar('\n');
+	return (0);
+}
