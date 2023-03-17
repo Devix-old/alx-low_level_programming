85097b2ce249fdf4272c62bd94a3eca8c3e6d561
devix-old <mohamed.elgharef@gmail.com>
2023-03-16 17:18:01 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\n
+			Size of a long long int: %d byte(s)\n
+Size of a float: %d byte(s)",sizeof(char),sizeof(int),sizeof(long int),sizeof(long long int),sizeof(float))
+	return (0);
+}
