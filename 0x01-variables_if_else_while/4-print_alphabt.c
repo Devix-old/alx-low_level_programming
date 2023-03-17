243e3a8f324671b6f81cbd30c9acd979dc9988b9
devix-old <mohamed.elgharef@gmail.com>
2023-03-17 10:45:26 -0700

    0x00. C - Hello, World

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there*/
+/**
+ * main - print alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char str[] = "abcdfghijklmnoprstuvwxyz";
+int i = 0;
+for (i = 0 ; i < 24 ; i++)
+{
+putchar (str[i]);
+}
+putchar('\n');
+	return (0);
+}
