999e498740da8fa684813b6e85c1a7c760cc4d79
devix-old <mohamed.elgharef@gmail.com>
2023-03-17 10:47:43 -0700

    0x00. C - Hello, World

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there*/
+/**
+ * main - print alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+for (i = 0 ; i < 10 ; i++){
+printf("%d",i);
+}
+return (0);
+}
