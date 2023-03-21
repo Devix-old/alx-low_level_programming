6a0decfe63fe39a9b1b91c288850845f34b8bedf
devix-old <mohamed.elgharef@gmail.com>
2023-03-21 15:18:17 -0700

    sss

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,23 +1,18 @@
-/* more headers goes there */
-/* betty style doc for function main goes there*/
+#include "main.h"
 /**
- * main - prints all alphabet in lower case x 10.
- *
- * Return: Always 0 (Success)
- */
-int main() {
-    
-int is_lower(int c){
-    if ((c >= 'a' && c <= 'z')||(c >= 'A' && c <= 'Z')){
-        return (1);
-	else{
-		return (0);
-	}
+* _isalpha - checks for alpha character.
+* @c: The character to be checked for lowercase.
+* Return: 1 if c is lowercase, 0 otherwise.
+*/
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+else
+{
+return (0);
 }
-
-}
-for (int i=0;i<=255;i++){
-    is_lower(i);
 }
 }
 
