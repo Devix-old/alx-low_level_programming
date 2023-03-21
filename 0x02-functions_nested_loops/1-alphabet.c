3bc28f7af856d335f1710f33ce94980104ba7fb7
devix-old <mohamed.elgharef@gmail.com>
2023-03-21 05:13:21 -0700

    sss

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,7 +10,6 @@
  */
 int main(void)
 {
-void print_alphabet(void);
 print_alphabet();
 return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,9 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+#ifndef main.h
+#define main.h
+
+void print_alphabet(void);
+
+#endif
