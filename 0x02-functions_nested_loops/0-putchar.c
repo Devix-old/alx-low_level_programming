0751c2cab4df3d6598482b7cd96c50004d0ee370
devix-old <mohamed.elgharef@gmail.com>
2023-03-21 04:37:01 -0700

    sss

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include "main.h"
 int main(void)
 {
-char c[] = "_putchar";
-putcha(c);
-return 0;
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
 
