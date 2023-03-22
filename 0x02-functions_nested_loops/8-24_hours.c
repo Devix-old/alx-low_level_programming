6058e8a109c002198a8e75dcea5d6e542c9282d1
devix-old <mohamed.elgharef@gmail.com>
2023-03-21 18:34:28 -0700

    sss

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,30 +1,27 @@
-#include <unistd.h>
-/* more headers goes there */
-#include <stdio.h>
-/* betty style doc for function main goes there*/
+#include "main.h"
 /**
- * main - prints all alphabet in lower case x 10.
- *
- * Return: Always 0 (Success)
- */
-
-void jack_bauer(void){
-	int x=0;
-	int y=0;
-while(y<24){
-    while (x<60){
-        putchar(y/10+'0');
-        putchar(y%10+'0');
-        putchar(':');
-        putchar(x/10+'0');
-        putchar(x%10+'0');
-        putchar('\n');
-
-        x++;
-    }
-    x=0;
-    y++;
+* jack_bauer - print 24 hours with seconds and minutes
+* @r: The integer value to be checked.
+* Return: return 0
+*/
+void jack_bauer(void)
+{
+int x = 0;
+int y = 0;
+while (y < 24)
+{
+while (x < 60)
+{
+putchar(y / 10 + '0');
+putchar(y % 10 + '0');
+putchar(':');
+putchar(x / 10 + '0');
+putchar(x % 10 + '0');
+putchar('\n');
+x++;
 }
-return(0);
+x = 0;
+y++;
 }
-
+return (0);
+}
