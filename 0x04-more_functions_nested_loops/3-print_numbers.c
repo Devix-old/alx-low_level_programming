dde755e1cd0da47de07489a2fcab511ace99a303
devix-old <mohamed.elgharef@gmail.com>
2023-03-23 02:23:41 -0700

    sss

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-  print_numbers - Prints the digits from 0 to 9 using _putchar.
+ * print_numbers - Prints the digits from 0 to 9 using .
  */
 void print_numbers(void)
 {
 int i;
-for (i = 0;i < 10;i++)
+for (i = 0; i < 10; i++)
 {
 _putchar(i + '0');
 }
