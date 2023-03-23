55f812a68e62b4c5e581a175bfeec581e2a6e039
devix-old <mohamed.elgharef@gmail.com>
2023-03-23 02:39:20 -0700

    sss

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_numbers - Prints the digits from 0 to 9 using _putchar.
+ * print_most_numbers - Prints the digits from 0 to 9 using _putchar.
  * exept 2 and 4
  */
 void print_most_numbers(void)
@@ -8,8 +8,10 @@ void print_most_numbers(void)
 int i;
 for (i = 0; i < 10; i++)
 {
-if ((i != 2) && (i != 4))
+if (i != 2 && i != 4)
+{
 _putchar(i + '0');
+}
 }
 _putchar('\n');
 }
