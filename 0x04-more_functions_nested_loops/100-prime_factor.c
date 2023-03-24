90d030a8a85131f5267229401ab53e14a8954134
devix-old <mohamed.elgharef@gmail.com>
2023-03-23 20:00:09 -0700

    sss

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,20 +3,17 @@
 
 /**
  * main - finds and prints the largest prime factor of the number 612852475143
- *
  * Return: Always 0
  */
 int main(void)
 {
 	long num = 612852475143;
 	long largest_factor = 0;
-
 	while (num % 2 == 0)
 	{
 		largest_factor = 2;
 		num /= 2;
 	}
-
 	for (long i = 3; i <= sqrt(num); i += 2)
 	{
 		while (num % i == 0)
@@ -25,13 +22,10 @@ int main(void)
 			num /= i;
 		}
 	}
-
 	if (num > 2)
 	{
 		largest_factor = num;
 	}
-
 	printf("%ld\n", largest_factor);
-
 	return (0);
 }
