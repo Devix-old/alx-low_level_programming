c1911002d0c387ee200dab60a03181069a29adb2
devix-old <mohamed.elgharef@gmail.com>
2023-03-27 14:14:06 -0700

    sss

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+/**
+ * print_array - Prints the elements of an integer array.
+ *
+ * @a: The integer array to be printed.
+ * @n: The number of elements in the array.
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d, ", a[i]);
+}
+printf("\n");
+}
