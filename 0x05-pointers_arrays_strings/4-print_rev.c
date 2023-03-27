3b4e58817a5992582df073dc41ba9c8918fb745c
devix-old <mohamed.elgharef@gmail.com>
2023-03-27 02:09:15 -0700

    sss

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev - Prints a string in reverse
+ * @s: The string to be printed
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int i;
+for (i = _strlen(s) - 1; i >= 0; i--)
+putchar(s[i]);
+putchar('\n');
+}}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#ifndef MAIN_H
+#define MAIN_H
+_strlen(char *s);
+#endif /* MAIN_H */
