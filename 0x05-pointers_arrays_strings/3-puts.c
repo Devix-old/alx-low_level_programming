c258360f76b0e3f744fcacf5ef8b0f5b1d218eee
devix-old <mohamed.elgharef@gmail.com>
2023-03-27 01:12:49 -0700

    sss

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+/**
+ * _puts - Prints a string
+ * @str: The string to be printed
+ */
+
+void _puts(char *str)
+{
+int i = 0;
+int x = 100;
+while (x != 0)
+{
+x = str[i];
+putchar(str[i]);
+i++;
+}
+}
