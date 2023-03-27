da955b9ebf110a0ed19e97dc80e1d2e5e604ab5a
devix-old <mohamed.elgharef@gmail.com>
2023-03-27 02:54:26 -0700

    sss

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,8 +9,8 @@ void rev_string(char *s)
 {
 int len = strlen(s);
 char temp;
-int i,j;
-for ( i = 0, j = len - 1; i < j; i++, j--)
+int i, j;
+for (i = 0, j = len - 1; i < j; i++, j--)
 {
 temp = s[i];
 s[i] = s[j];
