5436b1e499e41c82a41d6a77e67d2d8458642aa4
devix-old <mohamed.elgharef@gmail.com>
2023-03-28 23:10:10 -0700

    sss

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strncpy - copies the first n characters of the source string
+ * to the destination string, with null-termination.
+ * @dest: the destination string
+ * @src: the source string
+ * @n: the number of characters to copy
+ *
+ * Return: a pointer to the destination string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i, len_d, len_s;
+i = 0;
+len_s = strlen(src);
+len_d = strlen(dest);
+for (i = 0; i <= n - 1 && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+for (; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
+}
