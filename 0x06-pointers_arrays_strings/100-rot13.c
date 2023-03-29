07b0df4f0ce24ee44805d6f05dab2ff65ae38a27
devix-old <mohamed.elgharef@gmail.com>
2023-03-29 13:04:29 -0700

    sss

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * rot13 - Applies the rot13 substitution cipher to a string
+ *
+ * @str: Pointer to the string to be ciphered
+ *
+ * Return: Pointer to the ciphered string
+ */
+char *rot13(char *str)
+{
+char *ptr = str;
+int i, y, len;
+len = strlen(str);
+char alpha[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+for (i = 0; i < len; i++)
+{
+for (y = 0; y < 65 ; y++)
+{
+if (*ptr == alpha[y] || *ptr - 32 == alpha[y])
+{
+*ptr = alpha[y + 13];
+break;
+}
+}
+ptr++;
+}
+return (str);
+}
