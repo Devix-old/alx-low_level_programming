c9b31db2efe18c162cfe2599019f32e84bddb44d
devix-old <mohamed.elgharef@gmail.com>
2023-03-29 19:07:17 -0700

    sss

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdio.h>
+
 /**
  * rot13 - Applies the rot13 substitution cipher to a string
  *
@@ -11,13 +12,13 @@ char *rot13(char *str)
 {
 char *ptr = str;
 int i, y, len;
-len = strlen(str);
 char alpha[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+len = strlen(str);
 for (i = 0; i < len; i++)
 {
 for (y = 0; y < 65 ; y++)
