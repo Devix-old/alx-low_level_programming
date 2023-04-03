1970cf0b8babc3396058b6f3458b79374a9a5417
devix-old <mohamed.elgharef@gmail.com>
2023-04-03 07:42:15 -0700

    sss

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,7 +7,7 @@
  * Return: A pointer to the beginning of the located substring,
  *         or NULL if the substring is not found.
  */
-char *_strstr(char *haystack, char *needle)
+char *_strstr(char *haystack, char *needle){
 int i, j;
 int len_haystack = strlen(haystack);
 int len_needle = strlen(needle);
