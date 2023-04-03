cf9604ec8fc9b6dd6a2e9ff0a373218a3dced6c7
devix-old <mohamed.elgharef@gmail.com>
2023-04-03 14:49:48 -0700

    sss

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * diagonal_sum - calculates the sum of the diagonal elements in a square matrix
+ * @a: pointer to the first element of the matrix
+ * @size: size of the matrix
+ *
+ * Return: void
+ */
+void diagonal_sum(int *a, int size)
+{
+int sum1 = 0, sum2 = 0, i, y;
+for (i = 0, y = size - 1; i < size; i++, y--)
+{
+sum1 = *(a + size * i + i) + sum1;
+sum2 = *(a + size * i + y) + sum2;
+}
+printf("%d ", sum1);
+printf("%d", sum2);
+}
