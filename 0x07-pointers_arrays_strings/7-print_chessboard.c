51c17e50b7c2ad49a5f24b25c2a5d54228b996a8
devix-old <mohamed.elgharef@gmail.com>
2023-04-03 07:58:40 -0700

    sss

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * print_chessboard - prints a chessboard represented as a 2D array of characters
+ * @a: pointer to the 2D array
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+printf("%c", a[i][j]); 
+}
+printf("\n");
+}
+}
