e3c0bdb1b3b28e003904948cd6f10b9452ebedd8
devix-old <mohamed.elgharef@gmail.com>
2023-04-03 04:53:01 -0700

    sss

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,8 +1,7 @@
-#include <string.h>
 /**
- * _strchr - locate character in string
- * @s: pointer to string
- * @c: character to locate
+ * _strchr - locate a character in a string
+ * @s: the string to search
+ * @c: the character to find
  *
  * Return: pointer to the first occurrence of the character c in the string s,
  * or NULL if the character is not found
@@ -16,6 +15,10 @@ if (*s == c)
 return (s);
 }
 s++;
+}
+if (*s == c)
+{
+return (s);
 }
 return (NULL);
 }
