64e29d65f3fc527522daca41de666b60206f0a9e
devix-old <mohamed.elgharef@gmail.com>
2023-04-05 15:08:00 -0700

    0x08-recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,22 @@
+#include <string.h>
+/**
+ * helper - Checks if a string is a palindrome recursively
+ *
+ * @s: String to check
+ * @i: Starting index to check from
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+int helper(char *s, int i)
+{
+int len = strlen(s);
+if (i >= len)
+{
+return 1;
+}
+if (s[i] != s[len - 1 - i])
+{
+return 0;
+}
+return helper(s, i + 1);
+}
