6ddfafe50973dce5f9d0c870b57964df4a3f698c
devix-old <mohamed.elgharef@gmail.com>
2023-04-08 00:33:32 -0700

    0x08-recursion

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
  * main - calculates the minimum number of coins
  *        required to give a specific amount of change
  * @argc: the number of arguments passed to the program
  * @argv: an array of pointers to the arguments
- *
  * Return: 0 if successful, 1 if error
  */
 int main(int argc, char *argv[])
 {
 int coins = 0;
+int i;
+int t_coins[] = {25, 10, 5, 2, 1};
 int cent = atoi(argv[1]);
 if (argc != 2)
 {
@@ -24,32 +24,15 @@ printf("0\n");
 }
 else
 {
-while (cent / 25 > 0)
+for (i = 0; i < 5; i++)
 {
-cent = cent - 25;
+while (cent / t_coins[i] > 0)
+{
+cent = cent - t_coins[i];
 coins++;
 }
-while (cent / 10 > 0)
-{
-coins++;
-cent = cent - 10;
 }
-while (cent / 5 > 0)
-{
-coins++;
-cent = cent - 5;
-}
-while (cent / 2 > 0)
-{
-coins++;
-cent = cent - 2;
-}
-while (cent / 1 > 0)
-{
-coins++;
-cent = cent - 1;
-}
-printf("%d\n", coins);
+printf("%d", coins);
 }
 return (0);
 }
