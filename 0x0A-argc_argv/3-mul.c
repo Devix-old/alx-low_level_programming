d0109ef8bec3d50d4a0aac95113ed96b32ea721c
devix-old <mohamed.elgharef@gmail.com>
2023-04-07 23:12:56 -0700

    argc, argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies command line arguments and prints the result
+ * @argc: the number of command line arguments
+ * @argv: an array of strings containing the command line arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+int i;
+int mul = 1;
+for (i = 1; i < argc; i++) {
+mul = atoi(argv[i]) * mul;
+}
+printf("%d\n", mul);
+return (0);
+}
