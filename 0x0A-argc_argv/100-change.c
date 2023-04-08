28b711b42e73b253124963b1e0a2e7b89aee9414
devix-old <mohamed.elgharef@gmail.com>
2023-04-08 01:09:41 -0700

    0x08-recursion

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,19 +10,20 @@
 int main(int argc, char *argv[])
 {
 int coins = 0;
-int i,cent;
+int i, cent;
 int t_coins[] = {25, 10, 5, 2, 1};
-if (argc == 2 )
+if (argc == 2)
 {
 cent = atoi(argv[1]);
 }
-else{
+else
+{
 printf("Error\n");
 return (1);
 }
 if (cent < 0)
 {
-printf("%d\n",0);
+printf("%d\n", 0);
 }
 else
 {
