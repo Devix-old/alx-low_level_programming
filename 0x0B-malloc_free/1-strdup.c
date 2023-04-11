21b019fdded1039f1bc278f04b5e3bc29963f627
devix-old <mohamed.elgharef@gmail.com>
2023-04-11 07:34:55 -0700

    sss

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,7 +13,7 @@ char *_strdup(char *str)
 int i;
 int len = strlen(str);
 char *str_copy;
-str_copy = malloc(sizeof(char) * len);
+str_copy = malloc(sizeof(char) * len + 1);
 if (str_copy == NULL)
 {
 return (NULL);
