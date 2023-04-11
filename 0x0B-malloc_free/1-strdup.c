466d7f46b94e3eb6e7b6f8c10addeb868274a7fd
devix-old <mohamed.elgharef@gmail.com>
2023-04-11 07:52:29 -0700

    sss

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,14 +6,17 @@
  * _strdup - Duplicates a string
  * @str: Pointer to the input string
  *
- * Return: Pointer to the duplicated string
+ * Return: the duplicated string, or NULL if allocation fails
  */
 char *_strdup(char *str)
 {
+if (str == NULL)
+{
+return (NULL);
+}
 int i;
 int len = strlen(str);
-char *str_copy;
-str_copy = malloc(sizeof(char) * len + 1);
+char *str_copy = malloc(sizeof(char) * (len + 1));
 if (str_copy == NULL)
 {
 return (NULL);
@@ -22,5 +25,6 @@ for (i = 0; i < len; i++)
 {
 str_copy[i] = str[i];
 }
+str_copy[len] = '\0';
 return (str_copy);
 }
