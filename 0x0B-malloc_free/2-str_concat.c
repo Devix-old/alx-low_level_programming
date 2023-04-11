39d57f38d28a62b7f81cfcca55f7ee5031ea3d47
devix-old <mohamed.elgharef@gmail.com>
2023-04-11 09:00:47 -0700

    sss

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,68 +2,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * str_concat - Concatenates two strings
- * @s1: First string to concatenate
- * @s2: Second string to concatenate
- *
- * Return: A pointer to the newly allocated space in memory containing the
- * concatenated string, or NULL if an error occurred
- */
 char *str_concat(char *s1, char *s2)
 {
-        int len = 0, i = 0, j = 0;
-        char *concat;
+    int len = 0, i = 0, j = 0;
+    char *concat;
 
-        if (s1 == NULL && s2 == NULL)
-                return (NULL);
+    if (s1 == NULL && s2 == NULL )
+    {
+        return(NULL);
+    }
 
-        if (s1 != NULL)
+    if (s1 != NULL)
+    {
+        while (s1[i] != '\0')
         {
-                while (s1[i] != '\0')
-                {
-                        len++;
-                        i++;
-                }
+            len++;
+            i++;
         }
+    }
+    i = 0;
 
-        i = 0;
-        if (s2 != NULL)
+    if (s2 != NULL)
+    {
+        while (s2[i] != '\0')
         {
-                while (s2[i] != '\0')
-                {
-                        len++;
-                        i++;
-                }
+            len++;
+            i++;
         }
+    }
 
-        concat = malloc(sizeof(char) * (len + 1));
-        if (concat == NULL)
-                return (NULL);
+    concat = malloc(sizeof(char) * (len + 1));
 
-        i = 0;
-        if (s1 != NULL)
+    if (concat == NULL)
+    {
+        return(NULL);
+    }
+
+    i = 0;
+
+    if (s1 != NULL)
+    {
+        while (s1[i] != '\0')
         {
-                while (s1[i] != '\0')
-                {
-                        concat[j] = s1[i];
-                        i++;
-                        j++;
-                }
+            concat[j] = s1[i];
+            i++;
+            j++;
         }
+    }
 
-        i = 0;
-        if (s2 != NULL)
+    i = 0;
+
+    if (s2 != NULL)
+    {
+        while (s2[i] != '\0')
         {
-                while (s2[i] != '\0')
-                {
-                        concat[j] = s2[i];
-                        i++;
-                        j++;
-                }
+            concat[j] = s2[i];
+            i++;
+            j++;
         }
+    }
 
-        concat[j] = '\0';
-        return (concat);
+    concat[j] = '\0';
+
+    return(concat);
 }
 
