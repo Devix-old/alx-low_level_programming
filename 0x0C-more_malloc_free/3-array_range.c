5a9add2385c41e903769e13ac3b26bff86c25b06
devix-old <mohamed.elgharef@gmail.com>
2023-04-13 14:48:51 -0700

    sss

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,13 +12,19 @@ int *array_range(int min, int max)
 {
 int len, *tab, i;
 if (min > max)
+{
 return (NULL);
+}
 len = max - min;
-tab = malloc((len *sizeof(int)) + 1);
+tab = malloc((len * sizeof(int)) + 1);
 if (tab == NULL)
+{
 return (NULL);
+}
 for (i = 0; i <= len; i++)
+{
 tab[i] = min + i;
+}
 tab[i + 1] = '\0';
 return (tab);
 }
