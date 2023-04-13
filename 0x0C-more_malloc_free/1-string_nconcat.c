0553bd6399b7bd6bc0b7766b6c9360ba74e0adc4
devix-old <mohamed.elgharef@gmail.com>
2023-04-13 13:02:57 -0700

    sss

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,43 +1,34 @@
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: number of bytes of s2 to concatenate
+ * string_nconcat - Concatenates two strings
  *
- * Return: concatenated string, or NULL on failure
+ * @s1: The first string to concatenate
+ * @s2: The second string to concatenate
+ * @n: The maximum number of bytes of s2 to concatenate
+ *
+ * Return: A pointer to the concatenated string, or NULL on failure
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *tab;
-int len_s1, len_s2;
-len_s1 = strlen(s1);
-if (n > strlen(s2))
-{
-len_s2 = strlen(s2);
-}
-else
-{
-len_s2 = strnlen(s2, n);
-}
+char *concat;
+unsigned int len1, len2, concat_len;
 if (s1 == NULL)
-{
 s1 = "";
-}
 if (s2 == NULL)
-{
 s2 = "";
-}
-
-tab = malloc((len_s1 + len_s2 + 1) * sizeof(char));
-if (tab == NULL)
-{
+len1 = strlen(s1);
+len2 = strlen(s2);
+if (n >= len2)
+n = len2;
+concat_len = len1 + n + 1;
+concat = malloc(concat_len * sizeof(char));
+if (concat == NULL)
 return (NULL);
-}
-memcpy(tab, s1, len_s1);
-memcpy(tab + len_s1, s2, len_s2);
-tab[len_s2 + len_s1] = '\0';
-return (tab);
+memcpy(concat, s1, len1);
+memcpy(concat + len1, s2, n);
+concat[len1 + n] = '\0';
+return (concat);
 }
