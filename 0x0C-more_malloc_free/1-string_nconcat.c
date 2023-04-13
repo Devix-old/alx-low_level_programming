4394ca4f8ea6424255bc775326a0dcf69a8cef52
devix-old <mohamed.elgharef@gmail.com>
2023-04-13 08:45:00 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,6 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * string_nconcat - concatenates two strings
  * @s1: first string
@@ -12,33 +11,25 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *tab;
-    int len_s1, len_s2;
-
-    len_s1 = strlen(s1);
-    len_s2 = strnlen(s2, n);
-
-    if (s1 == NULL)
-    {
-        s1 = "";
-    }
-
-    if (s2 == NULL)
-    {
-        s2 = "";
-    }
-
-    tab = malloc((len_s1 + n + 1) * sizeof(char));
-
-    if (tab == NULL)
-    {
-        return (NULL);
-    }
-
-    memcpy(tab, s1, len_s1);
-    memcpy(tab + len_s1, s2, len_s2);
-    tab[len_s2 + len_s1] = '\0';
-
-    return (tab);
+char *tab;
+int len_s1, len_s2;
+len_s1 = strlen(s1);
+len_s2 = strnlen(s2, n);
+if (s1 == NULL)
+{
+s1 = "";
 }
-
+if (s2 == NULL)
+{
+s2 = "";
+}
+tab = malloc((len_s1 + n + 1) * sizeof(char));
+if (tab == NULL)
+{
+return (NULL);
+}
+memcpy(tab, s1, len_s1);
+memcpy(tab + len_s1, s2, len_s2);
+tab[len_s2 + len_s1] = '\0';
+return (tab);
+}
