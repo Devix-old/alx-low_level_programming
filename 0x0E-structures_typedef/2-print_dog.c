c7bfca3fd7754a4220511099411e645b18593d2a
devix-old <mohamed.elgharef@gmail.com>
2023-04-17 13:36:30 -0700

    3.14159265359

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,11 +8,18 @@
  */
 void print_dog(struct dog *d)
 {
+char NIL[5] = "(nil)";
 if (d == NULL)
-{
 return;
+if (d->name == NULL)
+d->name = NIL;
+if (d->owner == NULL)
+d->owner = NIL;
+if (d->age == 0)
+{
+printf("Name: %s\nAge: %s\nOwner: %s\n", d->name, NIL, d->owner);
 }
-if (d)
+else
 {
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
