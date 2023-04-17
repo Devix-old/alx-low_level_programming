a1fe70019dd38a17eb888bc5baff942c6c8de3b4
devix-old <mohamed.elgharef@gmail.com>
2023-04-17 13:11:34 -0700

    3.14159265359

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - prints information about a dog
+ * @d: pointer to a struct dog containing the dog's information
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+return;
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
