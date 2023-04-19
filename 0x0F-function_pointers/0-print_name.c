a9236999ce2d9098bc60ce13a56b2c9f23cdf304
devix-old <mohamed.elgharef@gmail.com>
2023-04-19 13:42:56 -0700

    sss

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,10 +2,10 @@
 /**
  * print_name - Prints a name using a function pointer
  * @name: Pointer to a character string representing the name
- * @f: Pointer to a function that takes a character string as argument and returns void
+ * @f: takes a character string as argument and returns void
  *
- * This function takes a character string pointer 'name' and a function pointer 'f',
- * and calls the function 'f' with 'name' as argument to print the name.
+ * takes a character string pointer 'name' and a function pointer 'f',
+ * calls the function 'f' with 'name' as argument to print the name.
  */
 void print_name(char *name, void (*f)(char *))
 {
