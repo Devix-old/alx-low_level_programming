3986bbf2c33e30949f3b5c9fe1d15e26d4131e24
devix-old <mohamed.elgharef@gmail.com>
2023-04-19 17:38:27 -0700

    sss

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,11 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
-
-
-int main(int argc, char *argv[]){
+/**
+ * main - Entry point for the program
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of strings containing the arguments passed to the program
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
 int num1, num2, result;
-if(argc > 4)
+if (argc > 4)
 {
 printf("erreur");
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,20 +1,60 @@
 #include "3-calc.h"
+
+/**
+ * op_add - Adds two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Result of the addition
+ */
 int op_add(int a, int b)
 {
 return (a + b);
 }
+
+/**
+ * op_sub - Subtracts two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Result of the subtraction
+ */
 int op_sub(int a, int b)
 {
 return (a - b);
 }
+
+/**
+ * op_mul - Multiplies two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Result of the multiplication
+ */
 int op_mul(int a, int b)
 {
 return (a * b);
 }
+
+/**
+ * op_div - Divides two integers
+ * @a: First integer (numerator)
+ * @b: Second integer (denominator)
+ *
+ * Return: Result of the division
+ */
 int op_div(int a, int b)
 {
 return (a / b);
 }
+
+/**
+ * op_mod - Computes the modulo of two integers
+ * @a: First integer (dividend)
+ * @b: Second integer (divisor)
+ *
+ * Return: Result of the modulo operation
+ */
 int op_mod(int a, int b)
 {
 return (a % b);
