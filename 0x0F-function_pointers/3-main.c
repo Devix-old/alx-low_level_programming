6be3baf38c0f0bf56e0483560cda6effde0d218a
devix-old <mohamed.elgharef@gmail.com>
2023-04-19 17:33:36 -0700

    sss

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,16 +5,14 @@
 
 
 int main(int argc, char *argv[]){
-int num1,num2,result;
+int num1, num2, result;
 if(argc > 4)
 {
 printf("erreur");
 }
-
 num1 = atoi(argv[1]);
 num2 = atoi(argv[3]);
 result = get_op_func(argv[2])(num1, num2);
-
-    printf("%d\n",result);
-    return(0);
+printf("%d\n", result);
+return (0);
 }
