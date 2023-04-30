6ed610c51f0c95395bce082bd1de30f865d8a72f
Devix-old <mohamed.elgharef@gmail.com>
2023-04-30 04:11:03 +0100

    sss

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,10 +1,13 @@
 #include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: pointer to the head of the linked list
+ */
+
 void free_listint(listint_t *head)
 {
-
 	listint_t *temp;
-
-	temp = head;
 
 	while (head != NULL)
 	{
