f40cb00b3b3f754af968b129723d631471532738
Devix-old <mohamed.elgharef@gmail.com>
2023-05-05 04:32:01 +0100

    sss

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: the number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int i = 31;
+	int first_one = 0;
+	if (n == 0)
+	{
+		putchar('0');
+		return;
+	}
+	while (i >= 0)
+	{
+		if ((n >> i) & 1)
+		{
+			putchar('1');
+			first_one++;
+		}
+		else if (first_one)
+			putchar('0');
+		i--;
+	}
+}
