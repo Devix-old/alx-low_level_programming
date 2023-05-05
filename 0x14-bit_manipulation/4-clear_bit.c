e14935303f32c8085b8579c75d0dd25fb4ae0fcb
Devix-old <mohamed.elgharef@gmail.com>
2023-05-05 08:00:03 +0100

    sss

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * cleat_bit - sets the value of a bit to 0 at a given index.
+ * clear_bit - sets the value of a bit to 0 at a given index.
  * @n: the number to get the bit from
  * @index: the index of the bit to sef 0
  * Return: 1 if it worked, or -1 if an error occurred
