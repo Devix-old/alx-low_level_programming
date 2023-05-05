971964e19c97f97720a75a6f9617fa6635571a6c
Devix-old <mohamed.elgharef@gmail.com>
2023-05-05 08:37:21 +0100

    sss

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,4 +1,3 @@
-#include "main.h"
 /**
  * flip_bits - returns the number of bits to flip to get from one number to another
  * @n: first number
@@ -9,7 +8,7 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int count = 0, i;
-
+	
 	for (i = 32; i > 0; i--)
 	{
 		if ((n & (1 << i)) != (m & (1 << i)))
