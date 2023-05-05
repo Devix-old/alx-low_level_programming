5bab6d73b4a61473b7abd83d9272885c0ecfc865
Devix-old <mohamed.elgharef@gmail.com>
2023-05-05 06:00:14 +0100

    sss

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,28 +1,15 @@
 #include "main.h"
 /**
  * print_binary - prints the binary representation of a number
+ *
  * @n: the number to print in binary
+ *
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
-	int i = 31;
-	int first_one = 0;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	while (i >= 0)
-	{
-		if ((n >> i) & 1)
-		{
-			_putchar('1');
-			first_one++;
-		}
-		else if (first_one)
-			_putchar('0');
-		i--;
-	}
+	_putchar((n & 1) + '0');
 }
-
