1cc9afb725eb6a8120df91e03c7abe234f2a6b9c
Devix-old <mohamed.elgharef@gmail.com>
2023-05-08 17:24:38 +0100

    first commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <string.h>
 #include <sys/stat.h>
+/**
+ * create_file - Creates a file and writes text to it
+ * @filename: Name of file to create
+ * @text_content: Text to write to file
+ *
+ * Return: 1 on success, -1 on failure
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int o, w, len;
