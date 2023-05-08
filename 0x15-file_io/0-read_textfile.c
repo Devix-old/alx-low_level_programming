3e8209cea4fb3e33b713c77de2ece733845c46d2
Devix-old <mohamed.elgharef@gmail.com>
2023-05-08 16:48:49 +0100

    first commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,19 +11,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buf;
 	int len_r, len_w, fd;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
 	if (filename == NULL)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
 		return (0);
+	fd = open(filename, O_RDONLY);
 
 	len_r = read(fd, buf, letters);
 	len_w = write(1, buf, len_r);
 
-	if (len_r == -1 || len_w == -1 || len_r != len_w)
+	if (len_r == -1 || len_w == -1 || fd == -1 || len_r != len_w)
 	{
 		free(buf);
 		return (0);
