8898de22d9590cc1a0096d2e94f18add89364cfb
Devix-old <mohamed.elgharef@gmail.com>
2023-05-08 17:34:42 +0100

    first commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,12 +17,15 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	len = strlen(text_content);
 	filemode = S_IRUSR | S_IWUSR;
-	o = open(filename, O_WRONLY | O_CREAT | O_EXCL, filemode);
+	o = open(filename, O_WRONLY | O_CREAT, filemode);
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
 		return (-1);
-
+	if (open(filename, O_EXCL) == -1)
+	{
+		truncate(filename, w);
+	}
 	return (1);
 	close(o);
 }
