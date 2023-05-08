68a9f394860eb451d03cb4136e88bfa04e488dca
Devix-old <mohamed.elgharef@gmail.com>
2023-05-08 22:08:44 +0100

    sss

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,20 +11,25 @@
 int cp_(const char *file_from, const char *file_to)
 {
 	int o, r;
-	int o_to, w_to;
+	int o_to, w_to, i = 0;
 	char buf[1024];
-
+	if (file_from == NULL || file_to == NULL)
+		return (0);
 	o = open(file_from, O_RDWR);
 	r = read(o, buf, 1024);
-
-	if (o == -1 || r == -1)
+	while (buf[i])
+	{
+		i++;
+	}
+	if (r == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 
 	o_to = open(file_to, O_WRONLY | O_TRUNC);
-	w_to = write(o_to, buf, 1024);
+
+	w_to = write(o_to, buf, i - 1);
 	if (o_to == -1 || w_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
@@ -32,13 +37,13 @@ int cp_(const char *file_from, const char *file_to)
 	}
 	if (close(o) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", o);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", o);
 		exit(100);
 	}
 
 	if (close(o_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", o_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", o_to);
 		exit(100);
 	}
 	return (0);
@@ -54,7 +59,7 @@ int main(int ac, const char **av)
 {
 	int rs;
 
-	if (ac != 2)
+	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
