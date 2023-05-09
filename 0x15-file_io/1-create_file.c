a9ac2236ef630fa10aea141dca6af2e602b9fd09
Devix-old <mohamed.elgharef@gmail.com>
2023-05-09 01:19:41 +0100

    sss

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,17 +15,22 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	len = strlen(text_content);
-
 	o = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (o == -1)
 	{
 		close(o);
 		return (-1);
 	}
-	if (text_content != NULL)
-		w = write(o, text_content, len);
 
+	if (text_content != NULL)
+	{
+		while (*text_content)
+		{
+			len++;
+			text_content++;
+		}
+		w = write(o, text_content, len);
+	}
 	if (w == -1)
 	{
 		close(o);
