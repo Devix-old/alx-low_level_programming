5737c168c5abd722e52769bb27d397b8dc2184fe
Devix-old <mohamed.elgharef@gmail.com>
2023-05-09 01:23:55 +0100

    sss

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,10 +9,10 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w;
-	int len;
+	int o, w, len = 0;
 
-	len = strlen(text_content);
+	while (text_content[len])
+		len++;
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
