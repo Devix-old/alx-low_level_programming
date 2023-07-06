f4e3002f40133ef746871e2e1a3b470c87dcaaad
Devix-old <mohamed.elgharef@gmail.com>
2023-07-06 23:10:23 +0100

    Task 2 completed

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * get_bit - function to get the value of a bit at a given index
+ * @n: the decimal to check
+ * @index: the given index
+ * Return: the value of the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+
+	if ((n >> index) & 1)
+		return (1);
+	else
+		return (0);
+
+	return (-1);
+}
