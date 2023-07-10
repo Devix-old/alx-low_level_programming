4ca6b05b40dd39e6800ba2276748999c93ef815c
Devix-old <mohamed.elgharef@gmail.com>
2023-07-10 14:56:04 +0100

    Task 1 Task 2 task 3 first commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,11 +11,15 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-
 	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, strlen(text_content));
-
-	if ((o == -1) || (w == -1))
+	if (o == -1)
 		return (-1);
+	if (text_content != NULL)
+	{
+		w = write(o, text_content, strlen(text_content));
+		if (w == -1)
+			return (-1);
+	}
+	close(o);
 	return (1);
 }
