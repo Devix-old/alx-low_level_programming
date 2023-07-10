94021f45c960afabe46e794fbe065f9c8281fed8
Devix-old <mohamed.elgharef@gmail.com>
2023-07-10 14:44:54 +0100

    Task 1 Task 2 task 3 first commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,10 +14,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	o = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, 0600);
+	if (o == -1)
+		return (-1);
 
 	w = write(o, text_content, strlen(text_content));
-
-	if ((o == -1) || (w == -1))
+	if (w == -1)
 		return (-1);
 
 	close(o);
