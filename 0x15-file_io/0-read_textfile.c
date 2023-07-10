74b001672260427725a8ceb7ff58160ddb20cb8a
Devix-old <mohamed.elgharef@gmail.com>
2023-07-10 11:46:44 +0100

    Task 1 Task 2 task 3 first commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,8 +17,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buf = malloc(letters * sizeof(char));
 
 	if (buf == NULL)
+	{
+		free(buf);
+		return (0);
+	}
 
-		o = open(filename, O_RDONLY);
+	o = open(filename, O_RDONLY);
 
 	r = read(o, buf, letters);
 
