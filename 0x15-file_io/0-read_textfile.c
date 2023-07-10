d3a3c6d78a9e3215b45cc04afa8f1e8a09c0e021
Devix-old <mohamed.elgharef@gmail.com>
2023-07-10 14:37:56 +0100

    Task 1 Task 2 task 3 first commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,37 @@
 #include "main.h"
 /**
- * read_textfile - This function open, read a file and write his content
- * @filename: Name of the file
- * @letters: Number of letters to read and write
- * Return: returns the actual number of letters it could read and print
+ * read_textfile - function read and write the content a file
+ * @filename: Name of the file to open.
+ * @letters: Number of letters to print in the standar ouput.
+ * Return: number of letter writed.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-
-	int o, r, w, c;
+	int r, w, o;
 	char *buf;
 
 	if (filename == NULL)
 		return (0);
-
-	buf = malloc(letters * sizeof(char));
-
-	if (buf == NULL)
-	{
-		free(buf);
-		return (0);
-	}
-
 	o = open(filename, O_RDONLY);
-
+	if (o == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
 	r = read(o, buf, letters);
-
-	w = write(0, buf, strlen(buf));
-
-	if ((o == -1) || (r == -1) || (w == -1))
-	{
-		close(o);
-		free(buf);
-		return (0);
-	}
-	c = close(o);
-	if (c == -1)
+	if (r == -1)
 	{
 		free(buf);
 		return (0);
 	}
-
+	w = write(STDOUT_FILENO, buf, r);
+	if (w == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	close(o);
 	free(buf);
 	return (w);
 }
