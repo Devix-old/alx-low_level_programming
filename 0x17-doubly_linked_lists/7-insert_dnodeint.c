b051629337731984cdf8fed0faadc548c6d24281
Devix-old <mohamed.elgharef@gmail.com>
2023-08-18 01:22:39 +0100

    Correction task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * insert_dnodeint_at_index - Inserts a new node at a given position.
  * @h: Pointer to a pointer to the head of the list.
@@ -9,32 +10,34 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current, *next, *new_node;
+	dlistint_t *new_node, *temp;
+	unsigned int i;
 
-	current = *h;
+	if (h == NULL)
+		return (NULL);
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	while (current != NULL && idx > 1)
-	{
-		current = current->next;
-		idx--;
-	}
-	if (idx != 0)
+
+	temp = *h;
+	for (i = 0; temp != NULL && i < idx - 1; i++)
+		temp = temp->next;
+
+	if (temp == NULL)
 		return (NULL);
-	if (current->next == NULL)
+
+	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
-	next = current->next;
 
 	new_node = malloc(sizeof(dlistint_t));
-
 	if (new_node == NULL)
 		return (NULL);
-	new_node->n = n;
-	current->next = new_node;
-	new_node->prev = current;
-	new_node->next = next;
-	next->prev = new_node;
 
-	return (new_node);
+	new_node->n = n;
+	new_node->prev = temp;
+	new_node->next = temp->next;
+	temp->next->prev = new_node;
+	temp->next = new_node;
+
+	return new_node;
 }
