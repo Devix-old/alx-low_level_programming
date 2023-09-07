8690e9215977b6a5ae459e1b44190e414a9dcd21
Devix-old <mohamed.elgharef@gmail.com>
2023-09-07 22:56:05 +0100

    sss

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,11 +18,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 
-	table->array = calloc(size, sizeof(hash_node_t));
+	table->array = malloc(sizeof(hash_node_t *) * size);
 
-	table->size = size;
 	if (table->array == NULL)
 		return (NULL);
+
+	table->size = size;
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 
