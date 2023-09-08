6433eb8d442c60b7cd5a7e15278149c4acfac4a8
Devix-old <mohamed.elgharef@gmail.com>
2023-09-08 03:13:15 +0100

    Sss

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,23 +1,5 @@
 #include "hash_tables.h"
 /**
- * HashtableEmpty - check if the hash-table is empty
- * @ht: The hash-table that we want to print
- * Return: True if empty False if not
- */
-bool HashtableEmpty(const hash_table_t *ht)
-{
-	unsigned int i;
-
-	for (i = 0; i < ht->size; i++)
-	{
-		if (ht->array[i] != NULL)
-		{
-			return (false);
-		}
-	}
-	return (true);
-}
-/**
  * hash_table_print - Prints a hash table
  * @ht: The hash table to print
  */
@@ -27,11 +9,9 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned int i;
 	int counter;
 
-	if (HashtableEmpty(ht))
-	{
-		printf("{}\n");
+	if (ht == NULL)
 		return;
-	}
+
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
