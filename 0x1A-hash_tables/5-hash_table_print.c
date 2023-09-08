2fc416d961e2996fc37fcb07f908c174dcdf2ff6
Devix-old <mohamed.elgharef@gmail.com>
2023-09-08 03:11:08 +0100

    Sss

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -41,7 +41,7 @@ void hash_table_print(const hash_table_t *ht)
 			if (counter > 0)
 				printf(", ");
 
-			printf("\'%s\' : \'%s\'", temp->key, temp->value);
+			printf("\'%s\': \'%s\'", temp->key, temp->value);
 			temp = temp->next;
 			counter++;
 		}
