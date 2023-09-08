893426c3cc70c3a7e48f04d566abd5b67235a764
Devix-old <mohamed.elgharef@gmail.com>
2023-09-08 02:01:20 +0100

    Sss

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || strcmp(key, "") == 0 || value == NULL)
 		return (0);
-	index = key_index(key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	/*case of empty slot */
 	if (ht->array[index] == NULL)
 	{
