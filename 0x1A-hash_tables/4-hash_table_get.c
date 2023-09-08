5bfac0b53670a79ffa1d5aea09ae572a7b15faf3
Devix-old <mohamed.elgharef@gmail.com>
2023-09-08 02:21:11 +0100

    Task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,7 +11,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	int index;
 	hash_node_t *temp;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
