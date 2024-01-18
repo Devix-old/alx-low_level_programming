1a84debc53b50bd6c149f0be12dfb592604362ba
Devix-old <mohamed.elgharef@gmail.com>
2024-01-18 18:35:56 +0100

    git add .!

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,4 +10,20 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary_recursive(int *array, size_t start, size_t end, int value);
 int advanced_binary(int *array, size_t size, int value);
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
 #endif
